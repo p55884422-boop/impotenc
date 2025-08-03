2d8eb073162d9d79ae9a5c3f63c36848a4eed155
p55884422-boop <p55884422@gmail.com>
2025-08-03 23:22:30 +0300

    Update تحويل_تحويل_تحويل.py

--- تحويل_تحويل_تحويل.py
+++ تحويل_تحويل_تحويل.py
@@ -1,15 +1,1 @@
-import sys, os,datetime
 
-d = """print('تم التحويل من قبل الثنائي عبدالله و احمد 👑')
-import sys,builtins,os
-def i(*a,**k): return f"exit({a},{k})\\n"
-sys.exit=i;os._exit=i;exitSystem=i;exit=i;quit=i
-class Q:
- def __init__(self,n):self.n=n
- def __repr__(self):return f"'{self.n}' is blocked"
- def __call__(self,*a,**k):i(*a,**k)
-builtins.exit=Q("exit");builtins.quit=Q("quit");builtins.exitSystem=i
-"""
-
-path = sys.modules['datetime'].__file__;u = path.replace('datetime.py','sitecustomize.py')
-with open(u,'w') as h:h.write(d)
