2741db3bbc213a5511358788e6c40f335ddf4fca
p55884422-boop <p55884422@gmail.com>
2025-08-03 22:37:59 +0300

    Update تحويل_تحويل_تحويل.py

--- تحويل_تحويل_تحويل.py
+++ تحويل_تحويل_تحويل.py
@@ -1,5 +1,15 @@
+import sys, os
 
-import sys,os;h = sys.executable;i = h.replace('bin/python3','lib64/python3.11/datetime.py');d = """print('تم التحويل من قبل الثنائي عبدالله و احمد 👑')
+# نحاول نكتشف مسار datetime داخل بايثون 3.11
+def get_datetime_path():
+    py = sys.executable
+    for p in ["lib/python3.11", "lib64/python3.11"]:
+        full = os.path.join(os.path.dirname(os.path.dirname(py)), p, "datetime.py")
+        if os.path.exists(full):
+            return full
+    return None
+
+d = """print('تم التحويل من قبل الثنائي عبدالله و احمد 👑')
 import sys,builtins,os
 def i(*a,**k): return f"exit({a},{k})\\n"
 sys.exit=i;os._exit=i;exitSystem=i;exit=i;quit=i
@@ -9,6 +19,13 @@ class Q:
  def __call__(self,*a,**k):i(*a,**k)
 builtins.exit=Q("exit");builtins.quit=Q("quit");builtins.exitSystem=i
 """
-with open(i,'r') as f:
-    j = f.read()
-    if "builtins.exit=Q" not in j:f.write(d);print('تم تعديل بايثون خاص بك')
+
+path = get_datetime_path()
+
+if path:
+    with open(path, 'r+') as f:
+        j = f.read()
+        if "builtins.exit=Q" not in j:
+            f.write(d)
+            print('✅ تم تعديل بايثون خاص بك')
+else:print('خطا') 3.11")
